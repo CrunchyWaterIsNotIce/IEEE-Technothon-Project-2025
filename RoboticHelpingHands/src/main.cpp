58045f4eadcc158d7f0bdacf0011d37a6db2581c
CrunchyWaterIsNotIce <lviray2006@gmail.com>
2025-11-06 14:38:46 -0600

    3D arm was fulling printed and wanted to test dual gesture sensors. Will have to edit library to accept the TwoWire attribute.

--- RoboticHelpingHands/src/main.cpp
+++ RoboticHelpingHands/src/main.cpp
@@ -1,138 +1,82 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <ESP32Servo.h>
 #include <SparkFun_APDS9960.h>
 
-SparkFun_APDS9960 gesture_sensor = SparkFun_APDS9960();
+TwoWire I2C_left = TwoWire(0);
+TwoWire I2C_right = TwoWire(1);
 
-// found in SparkFun_APDS9960.h
-#define APDS9960_PDATA   0x9C  // proximity data register
-#define APDS9960_GFLVL   0xAE  // FIFO level, how much data is waiting
-#define APDS9960_GFIFO_U 0xFC  // FIFO UP value
-#define APDS9960_GFIFO_D 0xFD  // FIFO DOWN value
-#define APDS9960_GFIFO_L 0xFE  // FIFO LEFT value
-#define APDS9960_GFIFO_R 0xFF  // FIFO RIGHT value
+SparkFun_APDS9960 left_apds = SparkFun_APDS9960();
+SparkFun_APDS9960 right_apds = SparkFun_APDS9960();
 
-const int SAMPLE_TIME = 2000; // 2 s
-const String NAME_OF_GESTURE = "palm_up";
-const int SAMPLE_DELAY = 50; // 50 ms
-const int NUM_SAMPLES = SAMPLE_TIME / SAMPLE_DELAY;
+const int LEFT_SCL_PIN = 17;
+const int LEFT_SDA_PIN = 16;
+const int LEFT_INTERUPT_PIN = 23;
+volatile bool left_isr_flag = false;
 
-bool readRegister(uint8_t, uint8_t&);
-void handleTrainingData();
+const int RIGHT_SCL_PIN = 22;
+const int RIGHT_SDA_PIN = 21;
+const int RIGHT_INTERUPT_PIN = 19;
+volatile bool right_isr_flag = false;
 
+void handleGesture(SparkFun_APDS9960&);
+void IRAM_ATTR leftInterruptRoutine();
+void IRAM_ATTR rightInterruptRoutine();
+ 
 void setup() {
-  // Initializes serial port number for Serial Monitor
-  Serial.begin(115200);
+    Serial.begin(115200);
+    I2C_left.begin(LEFT_SDA_PIN, LEFT_SCL_PIN, 100000);
+    I2C_right.begin(RIGHT_SDA_PIN, RIGHT_SCL_PIN, 100000);
 
-  // Initializes wires to I2C pins w/ fast speed (to keep I2C stable)
-  Wire.begin(21, 22, 100000);
-  
-  if (gesture_sensor.init()) Serial.println("apds is initialized"); // initializes sensor
-  else Serial.println("apds failed to initialize");
+    pinMode(LEFT_INTERUPT_PIN, INPUT_PULLUP);
+    pinMode(RIGHT_INTERUPT_PIN, INPUT_PULLUP);
 
-  // Tunes sensors down to stop saturation, default is too high
-  gesture_sensor.setGestureGain(GGAIN_2X);
-  gesture_sensor.setGestureLEDDrive(LED_DRIVE_25MA);
+    attachInterrupt(digitalPinToInterrupt(LEFT_INTERUPT_PIN), leftInterruptRoutine, FALLING);
+    attachInterrupt(digitalPinToInterrupt(RIGHT_INTERUPT_PIN), rightInterruptRoutine, FALLING);
 
-  if (gesture_sensor.enableGestureSensor(false)) Serial.println("gesture is initialized"); // initializes gesture
-  else Serial.println("gesture failed to initialize");
+    if (!left_apds.init()) Serial.println("Left sensor failed to initialize");
+    if (!right_apds.init()) Serial.println("Right sensor failed to initialize");
 
-  // Prompts custom gesture
-  Serial.print("Press ~ to run record custom gesture: ");
+    if (!left_apds.enableGestureSensor()) Serial.println("Left sensor gesture unavailable");
+    if (!right_apds.enableGestureSensor()) Serial.println("Right sensor gesture unavailable");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char c = Serial.read();
 
-    if (c == '~') {
-      while(Serial.available() > 0) Serial.read();
-
-      Serial.println();
-      Serial.printf("Now recording gesture [%s] for 2 seconds in...\n", NAME_OF_GESTURE);
-      for (int countdown = 3; countdown > 0; countdown--) {
-        Serial.printf("%d\n", countdown);
-        delay(1000);
-      }
-      Serial.println("GO");
-      delay(1000);
-
-      Serial.println();
-      Serial.println("proximity,up,down,left,right");
-      // for (int n = 0; n < NUM_SAMPLES; n++) {
-      //   handleTrainingData();
-      //   delay(SAMPLE_DELAY);
-      // }
-
-      // uses non-blocking timing from arduino, no delays except for sampling rate
-      unsigned long startTime = millis();
-      int sampleCount = 0;
-      
-      while (sampleCount < NUM_SAMPLES) {
-        handleTrainingData();
-        sampleCount++;
-        
-        unsigned long targetTime = startTime + (sampleCount * SAMPLE_DELAY);
-        while (millis() < targetTime) {}
-      }
-
-      Serial.println();
-      Serial.println("Finished current recording.");
-      Serial.print("Press ~ to rerun recording: ");
-    }
-  }
 }
 
-
-bool readRegister(uint8_t reg, uint8_t &val) {
-  Wire.beginTransmission(APDS9960_I2C_ADDR);
-  Wire.write(reg);
-  if (Wire.endTransmission(false) != 0) { // no transmission sends a restart
-    return false; // sensor ACK error
-  }
-
-  Wire.requestFrom((uint8_t)APDS9960_I2C_ADDR, (uint8_t)1);
-
-  if (Wire.available()) {
-    val = Wire.read();
-    return true;
-  }
-  return false;
+void handleGesture(SparkFun_APDS9960& apds, const char* sensor_name) {
+    if (apds.isGestureAvailable()) {
+        Serial.print(sensor_name);
+        Serial.print(": \n");
+        switch (apds.readGesture()) {
+            case DIR_UP:
+                Serial.println("swiped UP");
+                break;
+            case DIR_DOWN:
+                Serial.println("swiped DOWN");
+                break;
+            case DIR_LEFT:
+                Serial.println("swiped LEFT");
+                break;
+            case DIR_RIGHT:
+                Serial.println("swiped RIGHT");
+                break;
+            case DIR_FAR:
+                Serial.println("swiped FAR");
+                break;
+            case DIR_NEAR:
+                Serial.println("swiped NEAR");
+                break;
+            default:
+                Serial.println("none");
+        }
+    }
 }
 
-void handleTrainingData() {
-  uint8_t fifo_level = 0;
-  uint8_t prox_val = 0;
-  uint8_t u_val, d_val, l_val, r_val;
+void IRAM_ATTR leftInterruptRoutine() {
+    left_isr_flag = true;
+}
 
-  if ( !readRegister(APDS9960_PDATA, prox_val) ) {
-    Serial.println("Error reading proximity");
-    prox_val = 0;
-  }
-  
-  if ( !readRegister(APDS9960_GFLVL, fifo_level) ) {
-    Serial.println("Error reading FIFO level");
-    fifo_level = 0;
-  }
-
-  if ( fifo_level > 0 ) {
-    if (!readRegister(APDS9960_GFIFO_U, u_val) ||
-        !readRegister(APDS9960_GFIFO_D, d_val) ||
-        !readRegister(APDS9960_GFIFO_L, l_val) ||
-        !readRegister(APDS9960_GFIFO_R, r_val)
-      ) { // reads only ONE set of fifos
-      Serial.println("Error reading FIFO data");
-      u_val = d_val = l_val = r_val = 0; // uses zeros if FIFO is empty
-    }
-
-    for (int i = 1; i < fifo_level; i++) { // drains REMAINING FIFO
-      uint8_t dummy;
-      readRegister(APDS9960_GFIFO_U, dummy);
-      readRegister(APDS9960_GFIFO_D, dummy);
-      readRegister(APDS9960_GFIFO_L, dummy);
-      readRegister(APDS9960_GFIFO_R, dummy);
-    }
-  }
-  Serial.printf("%u,%u,%u,%u,%u\n", prox_val, u_val, d_val, l_val, r_val);
+void IRAM_ATTR rightInterruptRoutine() {
+    right_isr_flag = true;
 }
